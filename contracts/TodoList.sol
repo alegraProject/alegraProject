22a9c2c7a9b9319d5d32a7013c684c9c2181d514
Anthony Terry Jr <antman@macbook-pro.myfiosgateway.com>
2021-12-05 21:11:38 -0500

    another one.

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 contract Todolist {
     uint public taskCount = 0;
 
+    struct Task {   //solidity is statically typed language
+       uint id;
+       string content;
+       bool completed; 
+    }
+
+    mapping(uint => Task) public tasks;   // mapping has a key value pair.  data type for a key (unsigned integer) and the task (struct Task).  This is a database
+
+    //use constructor function, it is called when the contract is first deployed
+    constructor() public {
+        createTask("Let's do this");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
