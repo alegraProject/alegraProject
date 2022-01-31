200cf2ffda3156e7d7f845281bfd3dbd41963dda
Anthony Terry Jr <antman@macbook-pro.myfiosgateway.com>
2022-01-31 12:42:50 -0500

    added comments to each js in the migration folder.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-//The state of the database changes from adding content to it.  deploying a contract changes the state of it.  A migration is needed to chan ge the state.
-
 const TodoList = artifacts.require("./TodoList.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TodoList);
 };
 
+//The state of the database changes from adding content to it.  deploying a contract changes the state of it.  A migration is needed to chan ge the state.
